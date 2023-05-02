2c0a874d2de04d6a22f4942735ecc4a17275f98d
kurut08 <kurut08@gmail.com>
2023-05-02 16:57:29 +0200

    Minor looks fixes
    Added 2 panels to surround combat and nonCombat panels, fixed borders, commented all scaling as it it causing issues while it's not finished, made GameWindow maximised by default

--- src/afafi/GameWindow.java
+++ src/afafi/GameWindow.java
@@ -4,6 +4,7 @@ package afafi;
 /*
  * TODO:
  * Resize logo when resizing window
+ * Make all components scalable
  */
 
 import javax.swing.JFrame;
@@ -58,6 +59,7 @@ public class GameWindow extends JFrame {
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(0, 0, 1920, 1080);
+		setExtendedState(this.getExtendedState() | JFrame.MAXIMIZED_BOTH);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
@@ -72,7 +74,6 @@ public class GameWindow extends JFrame {
 		sidePanel.setLayout(null);
 		
 		logoPanel = new JPanel();
-		logoPanel.setBorder(new LineBorder(new Color(0, 0, 0)));
 		logoPanel.setBounds(0, 0, sidePanel.getWidth(), sidePanel.getHeight()/8);
 		logoPanel.setBackground(sidePanel.getBackground());
 		sidePanel.add(logoPanel);
@@ -91,15 +92,23 @@ public class GameWindow extends JFrame {
 		characterNameLabel.setFont(new Font("Tahoma", Font.PLAIN, 28));
 		characterNamePanel.add(characterNameLabel);
 		
+		JPanel combatPanel = new JPanel();
+		combatPanel.setBorder(new LineBorder(new Color(0, 0, 0)));
+		combatPanel.setBounds(0, 180, 320, 295);
+		sidePanel.add(combatPanel);
+		combatPanel.setLayout(null);
+		
 		JPanel combatTitlePanel = new JPanel();
-		combatTitlePanel.setBounds(0, 180, sidePanel.getWidth(), 45);
-		sidePanel.add(combatTitlePanel);
+		combatTitlePanel.setBounds(0, 0, combatPanel.getWidth() - 1, 45);
+		combatPanel.add(combatTitlePanel);
 		
 		JLabel combatTitleLabel = new JLabel("Combat");
 		combatTitleLabel.setFont(new Font("Tahoma", Font.PLAIN, 28));
 		combatTitlePanel.add(combatTitleLabel);
 		
 		JPanel combatSelectionPanel = new JPanel();
+		combatSelectionPanel.setBounds(0, 45, combatPanel.getWidth() - 1, 250);
+		combatPanel.add(combatSelectionPanel);
 		combatSelectionPanel.addMouseListener(new MouseAdapter() 
 		{
 			@Override
@@ -108,12 +117,10 @@ public class GameWindow extends JFrame {
 				
 			}
 		});
-		combatSelectionPanel.setBounds(0, 225, sidePanel.getWidth(), 250);
 		combatSelectionPanel.setLayout(null);
-		sidePanel.add(combatSelectionPanel);
 		
 		JPanel combatAttackPanel = new JPanel();
-		combatAttackPanel.setBounds(0, 0, sidePanel.getWidth(), 32);
+		combatAttackPanel.setBounds(0, 0, combatSelectionPanel.getWidth(), 32);
 		combatAttackPanel.setLayout(null);
 		combatAttackPanel.setBackground(new Color(255, 0, 0));
 		combatSelectionPanel.add(combatAttackPanel);
@@ -130,21 +137,27 @@ public class GameWindow extends JFrame {
 		
 		JProgressBar attackProgressBar = new JProgressBar();
 		attackProgressBar.setBounds(attackNameLabel.getX() + attackNameLabel.getWidth(), combatAttackPanel.getHeight()/4
-				, combatAttackPanel.getWidth() - attackNameLabel.getX() - attackNameLabel.getWidth(), 16);
+				, combatAttackPanel.getWidth() - attackNameLabel.getX() - attackNameLabel.getWidth() - 10, 16);
 		combatAttackPanel.add(attackProgressBar);
 		
+		JPanel nonCombatPanel = new JPanel();
+		nonCombatPanel.setBorder(new LineBorder(new Color(0, 0, 0)));
+		nonCombatPanel.setBounds(0, 475, sidePanel.getWidth(), 295);
+		sidePanel.add(nonCombatPanel);
+		nonCombatPanel.setLayout(null);
+		
 		JPanel nonCombatTitlePanel = new JPanel();
-		nonCombatTitlePanel.setBounds(0, 475, sidePanel.getWidth(), 45);
-		sidePanel.add(nonCombatTitlePanel);
+		nonCombatTitlePanel.setBounds(0, 1, nonCombatPanel.getWidth() - 1, 45);
+		nonCombatPanel.add(nonCombatTitlePanel);
 		
 		JLabel nonCombatTitleLabel = new JLabel("Non-Combat");
 		nonCombatTitleLabel.setFont(new Font("Tahoma", Font.PLAIN, 28));
 		nonCombatTitlePanel.add(nonCombatTitleLabel);
 		
-		JPanel noncombatSelectionPanel = new JPanel();
-		noncombatSelectionPanel.setBounds(0, 520, sidePanel.getWidth(), 250);
-		sidePanel.add(noncombatSelectionPanel);
-		noncombatSelectionPanel.setLayout(null);
+		JPanel nonCombatSelectionPanel = new JPanel();
+		nonCombatSelectionPanel.setBounds(0, 45, nonCombatPanel.getWidth() - 1, 250);
+		nonCombatPanel.add(nonCombatSelectionPanel);
+		nonCombatSelectionPanel.setLayout(null);
 		
 		JPanel woodcuttingPanel = new JPanel();
 		woodcuttingPanel.addMouseListener(new MouseAdapter() 
@@ -156,8 +169,8 @@ public class GameWindow extends JFrame {
 			}
 		});
 		woodcuttingPanel.setBackground(new Color(255, 0, 0));
-		woodcuttingPanel.setBounds(0, 0, sidePanel.getWidth(), 32);
-		noncombatSelectionPanel.add(woodcuttingPanel);
+		woodcuttingPanel.setBounds(0, 0, nonCombatSelectionPanel.getWidth(), 32);
+		nonCombatSelectionPanel.add(woodcuttingPanel);
 		woodcuttingPanel.setLayout(null);
 		
 		JLabel woodcuttingIconLabel = new JLabel("Icon");
@@ -173,7 +186,7 @@ public class GameWindow extends JFrame {
 		
 		JProgressBar woodcuttingProgressBar = new JProgressBar();
 		woodcuttingProgressBar.setBounds(woodcuttingNameLabel.getX() + woodcuttingNameLabel.getWidth(), woodcuttingPanel.getHeight()/4
-				, woodcuttingPanel.getWidth() - woodcuttingNameLabel.getX() - woodcuttingNameLabel.getWidth(), 16);
+				, woodcuttingPanel.getWidth() - woodcuttingNameLabel.getX() - woodcuttingNameLabel.getWidth() - 10, 16);
 		woodcuttingPanel.add(woodcuttingProgressBar);
 		
 		topPanel = new JPanel();
@@ -210,11 +223,10 @@ public class GameWindow extends JFrame {
 		shopLabel.setFont(new Font("Tahoma", Font.PLAIN, 28));
 		shopPanel.add(shopLabel);
 		
-		
+
 		contentPanel = new JPanel();
 		contentPanel = setContentPanel(contentPanel, "Summary");
-		contentPanel.setBounds(722, 307, 10, 10);
-		contentPanel.setBounds(sidePanel.getWidth(), topPanel.getHeight(),
+		contentPanel.setBounds(sidePanel.getWidth(), 60,
 				this.getWidth()-sidePanel.getWidth(), this.getHeight()-topPanel.getHeight());
 		contentPane.add(contentPanel);
 		
@@ -223,6 +235,7 @@ public class GameWindow extends JFrame {
 			@Override
 			public void componentResized(ComponentEvent e) 
 			{
+				/*
 				sidePanel.setBounds(0, 0, contentPane.getWidth()/6, contentPane.getHeight());
 				logoPanel.setBounds(0, 0, sidePanel.getWidth(), sidePanel.getHeight()/8);
 				
@@ -230,6 +243,7 @@ public class GameWindow extends JFrame {
 				topPanel.setBounds(sidePanel.getWidth(), 0, contentPane.getWidth() - sidePanel.getWidth(), contentPane.getHeight()/18);
 				contentPanel.setBounds(sidePanel.getWidth(), topPanel.getHeight(),
 						contentPane.getWidth()-sidePanel.getWidth(),contentPane.getHeight()-topPanel.getHeight());
+				*/
 			}
 		});
 	}
