f55db2fa3743307c392a77a32f3c0ace0c8906b7
kurut08 <kurut08@gmail.com>
2023-06-11 12:51:01 +0200

    Added empty equipment slot icons to equipment window

--- src/afafi/EquipmentWindow.java
+++ src/afafi/EquipmentWindow.java
@@ -1,10 +1,12 @@
 package afafi;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import java.awt.Color;
+import java.awt.Image;
 
 public class EquipmentWindow extends JFrame {
 
@@ -23,58 +25,58 @@ public class EquipmentWindow extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel headLabel = new JLabel("head");
-		headLabel.setBackground(new Color(255, 255, 255));
-		headLabel.setOpaque(true);
+		JLabel headLabel = new JLabel();
 		headLabel.setBounds(180, 10, 64, 64);
+		headLabel.setIcon(new ImageIcon(new ImageIcon(GameWindow.class.getResource("/afafi/images/icons/Empty_EQ/helmet_empty.png"))
+                .getImage().getScaledInstance(headLabel.getWidth(), headLabel.getHeight(), Image.SCALE_SMOOTH), "Head"));
 		contentPane.add(headLabel);
 		
-		JLabel chestLabel = new JLabel("chest");
-		chestLabel.setBackground(new Color(255, 255, 255));
-		chestLabel.setOpaque(true);
+		JLabel chestLabel = new JLabel();
 		chestLabel.setBounds(180, 80, 64, 64);
+		chestLabel.setIcon(new ImageIcon(new ImageIcon(GameWindow.class.getResource("/afafi/images/icons/Empty_EQ/chestplate_empty.png"))
+                .getImage().getScaledInstance(chestLabel.getWidth(), chestLabel.getHeight(), Image.SCALE_SMOOTH), "Chest"));
 		contentPane.add(chestLabel);
 		
 		JLabel legsLabel = new JLabel("legs");
-		legsLabel.setOpaque(true);
-		legsLabel.setBackground(new Color(255, 255, 255));
 		legsLabel.setBounds(180, 150, 64, 64);
+		legsLabel.setIcon(new ImageIcon(new ImageIcon(GameWindow.class.getResource("/afafi/images/icons/Empty_EQ/legs_empty.png"))
+                .getImage().getScaledInstance(legsLabel.getWidth(), legsLabel.getHeight(), Image.SCALE_SMOOTH), "Legs"));
 		contentPane.add(legsLabel);
 		
 		JLabel feetLabel = new JLabel("feet");
-		feetLabel.setOpaque(true);
-		feetLabel.setBackground(new Color(255, 255, 255));
 		feetLabel.setBounds(180, 220, 64, 64);
+		feetLabel.setIcon(new ImageIcon(new ImageIcon(GameWindow.class.getResource("/afafi/images/icons/Empty_EQ/boots_empty.png"))
+                .getImage().getScaledInstance(feetLabel.getWidth(), feetLabel.getHeight(), Image.SCALE_SMOOTH), "Boots"));
 		contentPane.add(feetLabel);
 		
 		JLabel weaponLabel = new JLabel("weapon");
-		weaponLabel.setBackground(new Color(255, 255, 255));
-		weaponLabel.setOpaque(true);
 		weaponLabel.setBounds(110, 80, 64, 64);
+		weaponLabel.setIcon(new ImageIcon(new ImageIcon(GameWindow.class.getResource("/afafi/images/icons/Empty_EQ/weapon_empty.png"))
+                .getImage().getScaledInstance(weaponLabel.getWidth(), weaponLabel.getHeight(), Image.SCALE_SMOOTH), "Weapon"));
 		contentPane.add(weaponLabel);
 		
 		JLabel shieldLabel = new JLabel("shield");
-		shieldLabel.setOpaque(true);
-		shieldLabel.setBackground(new Color(255, 255, 255));
 		shieldLabel.setBounds(250, 80, 64, 64);
+		shieldLabel.setIcon(new ImageIcon(new ImageIcon(GameWindow.class.getResource("/afafi/images/icons/Empty_EQ/shield_empty.png"))
+                .getImage().getScaledInstance(shieldLabel.getWidth(), shieldLabel.getHeight(), Image.SCALE_SMOOTH), "Shield"));
 		contentPane.add(shieldLabel);
 		
 		JLabel amuletLabel = new JLabel("amulet");
-		amuletLabel.setOpaque(true);
-		amuletLabel.setBackground(new Color(255, 255, 255));
 		amuletLabel.setBounds(110, 10, 64, 64);
+		amuletLabel.setIcon(new ImageIcon(new ImageIcon(GameWindow.class.getResource("/afafi/images/icons/Empty_EQ/amulet_empty.png"))
+                .getImage().getScaledInstance(amuletLabel.getWidth(), amuletLabel.getHeight(), Image.SCALE_SMOOTH), "Amulet"));
 		contentPane.add(amuletLabel);
 		
 		JLabel glovesLabel = new JLabel("gloves");
-		glovesLabel.setOpaque(true);
-		glovesLabel.setBackground(new Color(255, 255, 255));
 		glovesLabel.setBounds(110, 150, 64, 64);
+		glovesLabel.setIcon(new ImageIcon(new ImageIcon(GameWindow.class.getResource("/afafi/images/icons/Empty_EQ/gauntlet_empty.png"))
+                .getImage().getScaledInstance(glovesLabel.getWidth(), glovesLabel.getHeight(), Image.SCALE_SMOOTH), "Gauntlet"));
 		contentPane.add(glovesLabel);
 		
 		JLabel ringLabel = new JLabel("ring");
-		ringLabel.setBackground(new Color(255, 255, 255));
-		ringLabel.setOpaque(true);
 		ringLabel.setBounds(250, 150, 64, 64);
+		ringLabel.setIcon(new ImageIcon(new ImageIcon(GameWindow.class.getResource("/afafi/images/icons/Empty_EQ/ring_empty.png"))
+                .getImage().getScaledInstance(ringLabel.getWidth(), ringLabel.getHeight(), Image.SCALE_SMOOTH), "Ring"));
 		contentPane.add(ringLabel);
 	}
 }
